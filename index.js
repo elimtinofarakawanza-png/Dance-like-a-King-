4e69de2f415b834fb25bd2b99d7f5e0336333090
elimtinofarakawanza-png <elimtinofarakawanza@gmail.com>
2026-03-05 17:35:11 +0000

    Add initial HTML structure in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="game">
+    <div id="hit-zone"></div>
+  </div>
+
+  <script src="game.js"></script>
+</body>
+</html>
